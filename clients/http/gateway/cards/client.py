99e4844a379899fa660529a0e7e9d3813669c0f6
Vladimir <duracellp@yandex.ru>
2025-09-04 19:11:46 +0300

    Исправлены замечания

--- clients/http/gateway/cards/client.py
+++ clients/http/gateway/cards/client.py
@@ -3,9 +3,16 @@ from httpx import Response
 from clients.http.client import HTTPClient
 
 
-class CreateCartRequestDict(TypedDict):
+class CreateVirtualCartRequestDict(TypedDict):
     """
-    Структура данных для создания новой физической/виртуальной карты.
+    Структура данных для создания новой виртуальной карты.
+    """
+    userId: str
+    accountId: str
+
+class CreatePhysicalCartRequestDict(TypedDict):
+    """
+    Структура данных для создания новой физической карты.
     """
     userId: str
     accountId: str
@@ -15,7 +22,7 @@ class CardsGatewayHTTPClient(HTTPClient):
     Клиент для взаимодействия с /api/v1/cards сервиса http-gateway.
     """
 
-    def issue_virtual_card_api(self, request) -> Response:
+    def issue_virtual_card_api(self, request: CreateVirtualCartRequestDict) -> Response:
         """
         Создание виртуальной карты.
         :param request: Словарь с данными новой виртуальной карты.
@@ -23,7 +30,7 @@ class CardsGatewayHTTPClient(HTTPClient):
         """
         return self.post("/api/v1/cards/issue-virtual-card", json=request)
 
-    def issue_physical_card_api(self, request) -> Response:
+    def issue_physical_card_api(self, request: CreatePhysicalCartRequestDict) -> Response:
         """
         Создание физической карты.
         :param request: Словарь с данными новой физической карты.
