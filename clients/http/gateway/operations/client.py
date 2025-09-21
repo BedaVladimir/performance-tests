4cd23e87d30d3c169fa4faceef36ff0befeae636
Vladimir <duracellp@yandex.ru>
2025-09-21 22:10:40 +0300

    Реализованы locust-билдеры для operations и documents

--- clients/http/gateway/operations/client.py
+++ clients/http/gateway/operations/client.py
@@ -1,6 +1,6 @@
 from httpx import Response, QueryParams
 from clients.http.client import HTTPClient, HTTPCClientExtensions
-from clients.http.gateway.client import build_gateway_http_client
+from clients.http.gateway.client import build_gateway_http_client, build_gateway_locust_http_client
 from clients.http.gateway.operations.schema import (
     MakeFeeOperationRequestSchema, MakeTopUpOperationRequestSchema,
     MakeCashbackOperationRequestSchema, MakeTransferOperationRequestSchema,
@@ -12,7 +12,7 @@ from clients.http.gateway.operations.schema import (
     GetOperationsSummaryQuerySchema, GetOperationsSummaryResponseSchema,
     GetOperationReceiptResponseSchema, GetOperationQuerySchema, GetOperationsResponseSchema
 )
-
+from locust.env import Environment
 
 class OperationsGatewayHTTPClient(HTTPClient):
     """
@@ -193,3 +193,16 @@ def build_operations_gateway_http_client() -> OperationsGatewayHTTPClient:
     :return: Готовый к использованию OperationsGatewayHTTPClient.
     """
     return OperationsGatewayHTTPClient(client=build_gateway_http_client())
+
+
+def build_operations_gateway_locust_http_client(environment: Environment) -> OperationsGatewayHTTPClient:
+    """
+    Функция создаёт экземпляр OperationsGatewayHTTPClient адаптированного под Locust.
+
+    Клиент автоматически собирает метрики и передаёт их в Locust через хуки.
+    Используется исключительно в нагрузочных тестах.
+
+    :param environment: объект окружения Locust.
+    :return: экземпляр OperationsGatewayHTTPClient с хуками сбора метрик.
+    """
+    return OperationsGatewayHTTPClient(client=build_gateway_locust_http_client(environment))
